06bec5a49ab9309276641681e5e59926468747b7
Andrey33392 <114940742+Andrey33392@users.noreply.github.com>
2023-02-03 11:58:44 +0300

    ver2

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -2,6 +2,7 @@ package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +12,7 @@ import ru.kata.spring.boot_security.demo.service.UserServiceImpl;
 
 import java.util.List;
 
-@Component
+@Controller
 public class AdminController {
 
     private final UserServiceImpl userService;
